0b571fdc20ee9cc14dda41775ed074268566ff65
Bruno N <brunonaponiello@gmail.com>
2016-11-22 00:00:58 -0200

    First commit

---
+++ src/main/java/br/com/personaltimeclock/PersonalTimeClockApplication.java
@@ -0,0 +1,12 @@
+package br.com.personaltimeclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTimeClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTimeClockApplication.class, args);
+	}
+}
